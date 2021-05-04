f9467f1ac2f9945e2e0d1af8fe66a689f71cf8f7
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2021-05-04 15:54:29 -0300

    Initial commit

---
+++ src/app/alunos/alunos.module.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+import { AlunosComponent} from './alunos.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+
+import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service'; 
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [        
+        CommonModule,
+        AlunosRoutingModule,
+        FormsModule
+    ],
+    // exports: [],
+    declarations: [
+        AlunosComponent, 
+        AlunoFormComponent, 
+        AlunoDetalheComponent
+    ]
+    , providers: [AlunosService]
+  
+})
+export class AlunosModule {}
